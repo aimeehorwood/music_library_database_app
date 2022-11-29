22f941f78f19484c17b8c0fa06266ae56606b9d5
Aimee <aimeehorwood@hotmail.com>
2022-11-29 12:07:33 +0000

    Adds Erb exercise

--- spec/integration/application_spec.rb
+++ spec/integration/application_spec.rb
@@ -10,6 +10,19 @@ describe Application do
   # class so our tests work.
   let(:app) { Application.new }
 
+
+  context "GET/albums/:id" do 
+    it "should return info about album 1" do 
+      response = get('/albums/2')
+
+      expect(response.status).to eq(200)
+      expect(response.body).to include('<h1> Surfer Rosa </h1>')
+      expect(response.body).to include('Release year: 1988')
+      expect(response.body).to include('Artist: Pixies')
+    end
+  end
+  
+
   context "GET/ albums" do
     it "should return the list of albums" do
       response = get('/albums')
