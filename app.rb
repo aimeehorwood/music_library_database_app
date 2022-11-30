57a3e88210a506d4551b2f87158810f46d066636
Aimee <aimeehorwood@hotmail.com>
2022-11-30 16:19:09 +0000

    ready to deploy:

--- app.rb
+++ app.rb
@@ -5,7 +5,6 @@ require_relative "lib/database_connection"
 require_relative "lib/album_repository"
 require_relative "lib/artist_repository"
 
-DatabaseConnection.connect
 
 class Application < Sinatra::Base
   configure :development do
